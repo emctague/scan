a215881fd368bb15eafc5769007e647cf2c1979e
Ethan McTague <ethan@tague.me>
2019-06-13 11:01:18 -0400

    fix?

--- gl/fragment.glsl
+++ gl/fragment.glsl
@@ -8,7 +8,7 @@ out vec4 FragColor;
 
 const vec3 lightPos = vec3(0.5, 6.0, 0.5);
 const vec3 objectColor = vec3(0.8, 0.5, 0.5);
-const float falloff = 0.2;
+const float falloff = 1.0;
 
 void
 main ()
